a05b3b745fe9c0dd5ea6d43a85af06f648e55a48
Konstantin Spitsin <konstantin.spicin@simbirsoft.com>
2022-07-09 20:07:56 +0300

    da2

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"os"
 	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
+	flag.Parse()
+	words := strings.Join(flag.Args(), "")
 
-	words := scanner.Text()
+	if len(words) == 0 {
+		fmt.Println(0)
+
+		return
+	}
 
 	fmt.Println(len(strings.Split(words, " ")))
 }
+
+//func readInput() (pattern, words string, err error) {
+//	flag.StringVar(&pattern, "p", "", "pattern to match against")
+//	flag.Parse()
+//	if pattern == "" {
+//		return pattern, src, errors.New("missing pattern")
+//	}
+//	src = strings.Join(flag.Args(), "")
+//	if src == "" {
+//		return pattern, src, errors.New("missing string to match")
+//	}
+//	return pattern, src, nil
+//}
